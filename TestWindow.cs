eec17a3960c230f793b572a81c9e1cc3dfe8bd25
EliBot68 <eliasalas279@gmail.com>
2025-09-30 02:17:25 +0200

    Batch 7: WPF Test Application files

---
+++ TestWindow.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TestWindow
+{
+    public partial class TestWindow : Window
+    {
+        public TestWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
